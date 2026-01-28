0cbed3341a3e1c6a878f589ee5f143150839d737
anjilinux <anjireddy2024@gmail.com>
2026-01-28 23:08:39 +0530

    add data

--- train.py
+++ train.py
@@ -29,6 +29,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Ensure model directory exists
 os.makedirs("models", exist_ok=True)
 
+mlflow.set_experiment("animal-linear-reg")
 # Train + MLflow tracking
 with mlflow.start_run():
     model = LinearRegression()
@@ -44,91 +45,3 @@ with mlflow.start_run():
     mlflow.log_artifact("models/model.pkl")
 
 
-
-
-
-
-
-
-
-# import os
-# import mlflow
-# import mlflow.sklearn
-# import pandas as pd
-# from sklearn.linear_model import LinearRegression
-# from sklearn.model_selection import train_test_split
-# import joblib
-
-# # Load data
-# data = pd.read_csv("clean_data.csv")
-
-# # Clean column names (VERY important for CI)
-# data.columns = data.columns.str.strip()
-
-# print("Columns:", list(data.columns))  # helps debug Jenkins
-
-# # Validate target column
-# if "target" not in data.columns:
-#     raise ValueError("❌ 'target' column not found in clean_data.csv")
-
-# X = data.drop("target", axis=1)
-# y = data["target"]
-
-# # Split
-# X_train, X_test, y_train, y_test = train_test_split(
-#     X, y, test_size=0.2, random_state=42
-# )
-
-# # Create model directory
-# os.makedirs("models", exist_ok=True)
-
-# # Train + track
-# with mlflow.start_run():
-#     model = LinearRegression()
-#     model.fit(X_train, y_train)
-
-#     joblib.dump(model, "models/model.pkl")
-
-#     mlflow.log_param("model_type", "LinearRegression")
-#     mlflow.sklearn.log_model(model, "model")
-#     mlflow.log_artifact("models/model.pkl")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import mlflow
-# import mlflow.sklearn
-# import pandas as pd
-# from sklearn.linear_model import LinearRegression
-# from sklearn.model_selection import train_test_split
-# import joblib
-
-# data = pd.read_csv("clean_data.csv")
-
-# X = data.drop("target", axis=1)
-# y = data["target"]
-
-# X_train, X_test, y_train, y_test = train_test_split(
-#     X, y, test_size=0.2, random_state=42
-# )
-
-# with mlflow.start_run():
-#     model = LinearRegression()
-#     model.fit(X_train, y_train)
-
-#     joblib.dump(model, "models/model.pkl")
-
-#     mlflow.log_param("model_type", "LinearRegression")
-#     mlflow.sklearn.log_model(model, "model")
-#     mlflow.log_artifact("models/model.pkl")
